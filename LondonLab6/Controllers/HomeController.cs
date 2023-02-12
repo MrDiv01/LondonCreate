33e2ed5e75fc715af72964dbb85479f6f091b189
Nurlan Memmedov <nurlan.memmedov8818@gmail.com>
2023-02-12 22:13:46 +0400

    Add project files.

---
+++ LondonLab6/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using LondonLab6.Data;
+using LondonLab6.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LondonLab6.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        public HomeController(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext= applicationDbContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(User user)
+        {
+            _applicationDbContext.Users.Add(user);
+            _applicationDbContext.SaveChanges();
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
---
+++ LondonLab6/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using LondonLab6.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LondonLab6.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options) { }
+        public DbSet<User> Users { get; set; }
+    }
+}
